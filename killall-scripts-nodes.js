772440bfef941650988183136f432f1bc6277f98
dennismartis <martisdennis@gmail.com>
2022-03-24 01:14:13 +0100

    prettier

--- killall-scripts-nodes.js
+++ killall-scripts-nodes.js
@@ -1,10 +1,10 @@
-import { getConnectedNodes } from './utils.js';
+import { getConnectedNodes } from "./utils.js";
 /** @param {NS} ns **/
 export async function main(ns) {
-	const connectedNodes = getConnectedNodes(ns)
-	for (let i = 0; i < connectedNodes.length; i++) {
-		if (connectedNodes[i] != 'home'){
-			ns.killall(connectedNodes[i])
-		}
-	}
+  const connectedNodes = getConnectedNodes(ns);
+  for (let i = 0; i < connectedNodes.length; i++) {
+    if (connectedNodes[i] != "home") {
+      ns.killall(connectedNodes[i]);
+    }
+  }
 }
--- purchase-servers.js
+++ purchase-servers.js
@@ -1,15 +1,14 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	var ram = ns.args[0] || 1024;
-	var i = 0;
+  var ram = ns.args[0] || 1024;
+  var i = 0;
 
-	while (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) {
-		var host
-		if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
-			var host = ns.purchaseServer("pserv-" + i, ram);
-			++i;
-		}
-		await ns.sleep(1000)
-
-	}
+  while (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) {
+    var host;
+    if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+      var host = ns.purchaseServer("pserv-" + i, ram);
+      ++i;
+    }
+    await ns.sleep(1000);
+  }
 }
