d438e458168ea9f7b676652fd97157c2a02731e9
dennismartis <martisdennis@gmail.com>
2022-03-26 22:21:25 +0100

    debugging

--- utils.js
+++ utils.js
@@ -3,13 +3,27 @@ export function getConnectedNodes(ns, host) {
   const scannedNodes = [host];
   for (const node of scannedNodes) {
     const scanList = ns.scan(node);
+    console.log(
+      "🚀 ~ file: utils.js ~ line 6 ~ getConnectedNodes ~ scanList",
+      scanList
+    );
     for (const scan of scanList) {
-      console.log(scan);
+      console.log(
+        "🚀 ~ file: utils.js ~ line 8 ~ getConnectedNodes ~ scan",
+        scan
+      );
       if (!scannedNodes.includes(scan)) {
         scannedNodes.push(scan);
-        console.log("test");
+        console.log(
+          "🚀 ~ file: utils.js ~ line 11 ~ getConnectedNodes ~ scan",
+          scan
+        );
       }
     }
   }
+  console.log(
+    "🚀 ~ file: utils.js ~ line 16 ~ getConnectedNodes ~ scannedNodes",
+    scannedNodes
+  );
   return scannedNodes;
 }
