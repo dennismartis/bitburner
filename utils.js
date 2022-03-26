72eacaac38682a0a48055ff63193c0e1e52b079b
dennismartis <martisdennis@gmail.com>
2022-03-26 22:36:19 +0100

    cleanup

--- utils.js
+++ utils.js
@@ -1,29 +1,15 @@
 /** @param {NS} ns **/
-export function getConnectedNodes(ns, host) {
+export function getConnectedNodes(ns, host = "home") {
   const scannedNodes = [host];
   for (const node of scannedNodes) {
     const scanList = ns.scan(node);
-    console.log(
-      "🚀 ~ file: utils.js ~ line 6 ~ getConnectedNodes ~ scanList",
-      scanList
-    );
     for (const scan of scanList) {
-      console.log(
-        "🚀 ~ file: utils.js ~ line 8 ~ getConnectedNodes ~ scan",
-        scan
-      );
       if (!scannedNodes.includes(scan)) {
         scannedNodes.push(scan);
-        console.log(
-          "🚀 ~ file: utils.js ~ line 11 ~ getConnectedNodes ~ scan",
-          scan
-        );
       }
     }
   }
-  console.log(
-    "🚀 ~ file: utils.js ~ line 16 ~ getConnectedNodes ~ scannedNodes",
-    scannedNodes
-  );
+  const home = scannedNodes.findIndex("home");
+  scannedNodes.splice(home, 1);
   return scannedNodes;
 }
