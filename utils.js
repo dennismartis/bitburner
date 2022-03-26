46a2ca039191f6db85ba6df1cd070b56953a57d8
dennismartis <martisdennis@gmail.com>
2022-03-26 22:18:32 +0100

    1

--- utils.js
+++ utils.js
@@ -4,6 +4,7 @@ export function getConnectedNodes(ns, host) {
   for (const node of scannedNodes) {
     const scanList = ns.scan(node);
     for (const scan of scanList) {
+      console.log(scan);
       if (!scannedNodes.includes(scan)) {
         scannedNodes.push(scan);
         console.log("test");
