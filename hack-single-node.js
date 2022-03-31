c3cef610805ad442f88362ef7dbef8f7dc27cd9b
dennismartis <martisdennis@gmail.com>
2022-03-31 14:06:07 +0200

    a

---
+++ hack-single-node.js
@@ -0,0 +1,13 @@
+// start with weaken script until security level is at minimal
+// calculate weaken time with getWeakenTime(host)
+// calculate threads needed to get security level to minimal
+
+// when security level is minimal start grow script
+// calculate grow time with getGrowTime(host)
+// Calculate the number of grow thread needed to grow a server by a certain multiplier using growthAnalyze(host, growthAmount, cores)
+// Calculate the security increase for a number of thread with growthAnalyzeSecurity(threads)
+// Calculate h
+// grow until server money is at 90% or higher of the maximum money possible
+// then hack the server.
+// grow server
+// hack server
